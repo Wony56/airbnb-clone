baba331a14418fca4ccc0bc9c769530733b7d917
Lee <blackmonkey9256@gmail.com>
2020-01-18 14:01:56 +0900

    #10 Pagination with django's Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,19 @@
 from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import models
 
 
 def home(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+    except ValueError:
+        return redirect("/")
+    except EmptyPage and PageNotAnInteger:
+        return redirect("/")
 
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    return render(request, "rooms/home.html", {"page": rooms})
+
