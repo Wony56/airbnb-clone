d6441ace1f980fddf6b068a077c151c1f17a00a7
Lee <blackmonkey9256@gmail.com>
2020-01-18 17:05:36 +0900

    #13 Room detail with CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,7 @@
 from django.utils import timezone
-from django.views.generic import ListView
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 from django.db.models import FieldDoesNotExist
 from django.urls import reverse
-from django.shortcuts import render
 from . import models
 
 
@@ -23,10 +21,8 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
 
+    """ Room Detail Definition """
+
+    model = models.Room
