775d2b71b1affed64e075269e738b9a3f7f1efe8
Lee <blackmonkey9256@gmail.com>
2020-01-18 15:07:51 +0900

    #11 HomeView using CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,19 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def home(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-    except ValueError:
-        return redirect("/")
-    except EmptyPage and PageNotAnInteger:
-        return redirect("/")
+class HomeView(ListView):
 
-    return render(request, "rooms/home.html", {"page": rooms})
+    """ HomeView Definition """
 
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
