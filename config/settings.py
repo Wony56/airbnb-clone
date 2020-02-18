cdb3e6003e2835ba29dbee55c3a8bd1e578b9ef6
Lee <blackmonkey9256@gmail.com>
2020-02-18 20:44:57 +0900

    #24.10.2 Modified STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -176,7 +176,7 @@ if not DEBUG:
     AWS_DEFAULT_ACL = 'public-read'
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     # Sentry
 
